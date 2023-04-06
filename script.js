810730aa8856fc56fa0d581c50b5168902a5669f
Krustyy <99592586+Krustyy03@users.noreply.github.com>
2023-04-06 14:55:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const doStuffs = () =>
-  fetch("https://Flake.krustyy.repl.co")
+  fetch("https://Wisk.krustyy.repl.co")
     .then((e) => e.json())
     .then((f) => {
       document.querySelector("#servers").innerHTML = "Used by "+f.guilds.toLocaleString()+" servers.";
       document.querySelector("#users").innerHTML = "Trusted by "+f.users.toLocaleString()+" users.";
     });
 doStuffs();
-setInterval(doStuffs, 3e4);
+setInterval(doStuffs, 10000);
 
